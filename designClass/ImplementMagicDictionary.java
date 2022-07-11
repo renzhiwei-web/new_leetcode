47f2c1bb134ad4446ca1661d7d3ff45481b853ab
renzhiwei-web <renzhiwei565@163.com>
2022-07-11 20:30:37 +0800

    简单的模拟，后续补充字典树的方法

---
+++ designClass/ImplementMagicDictionary.java
@@ -0,0 +1,36 @@
+package designClass;
+
+public class ImplementMagicDictionary {
+	
+	// leetcode 676 medium 2022/7/11
+	// 首先容易想到使用枚举方法
+	String[] words;
+    public ImplementMagicDictionary() { 
+
+    }
+    
+    public void buildDict(String[] dictionary) {
+    	words = dictionary;
+    }
+    
+    public boolean search(String searchWord) {
+    	for(String word : words) {
+    		if (word.length() != searchWord.length()) {
+				continue;
+			}
+    		int diff = 0;
+    		for(int i = 0;i < word.length();i++) {
+    			if(word.charAt(i) != searchWord.charAt(i)) {
+    				diff++;
+    			}
+    			if (diff > 1) {
+					break;
+				}
+    		}
+    		if (diff == 1) {
+				return true;
+			}
+    	}
+    	return false;
+    }
+}
