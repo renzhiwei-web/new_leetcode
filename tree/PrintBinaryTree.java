1f770e9f1b8145ce7d96bc61f7cafacd68eb8f64
renzhiwei-web <renzhiwei565@163.com>
2022-08-22 10:00:32 +0800

    leetcode 655 二叉树的性质

---
+++ tree/PrintBinaryTree.java
@@ -0,0 +1,56 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PrintBinaryTree {
+	// leetcode 655 medium 2022/8/22
+	// 先通过dfs获取到树的高度，再构造零矩阵，最后再通过dfs遍历树进行矩阵的填充
+	public List<List<String>> printTree(TreeNode root) {
+		// 获取高度
+		int height = getHeight(root);
+		String[][] ans = new String[height][(1 << height) - 1];
+		for (String[] arr : ans) {
+			Arrays.fill(arr, "");
+		}
+		List<List<String>> res = new ArrayList<>();
+		fill(ans, root, 0, 0, ans[0].length);
+		for (String[] arr : ans) {
+			res.add(Arrays.asList(arr));
+		}
+		return res;
+	}
+
+	/**
+	 * 填充数组
+	 *
+	 * @param ans  数组
+	 * @param root 根节点
+	 * @param i    第几行
+	 * @param l    左节点
+	 * @param r    右节点
+	 */
+	private void fill(String[][] ans, TreeNode root, int i, int l, int r) {
+		if (root == null) {
+			return;
+		}
+		ans[i][(l + r) / 2] = "" + root.val;
+		fill(ans, root.left, i + 1, l, (l + r) / 2); // 将每个节点看成根节点，根节点在每一层的中心点
+		fill(ans, root.right, i + 1, (l + r + 1) / 2, r);
+	}
+
+	/**
+	 * 获取树的高度
+	 *
+	 * @param root
+	 * @return
+	 */
+	private int getHeight(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+	}
+
+}
