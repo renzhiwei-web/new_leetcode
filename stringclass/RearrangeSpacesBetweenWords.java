c58386a0b450635388eb9e3c0772fe69e410261f
renzhiwei-web <renzhiwei565@163.com>
2022-09-07 09:52:12 +0800

    leetcode 1592 简单模拟

---
+++ stringclass/RearrangeSpacesBetweenWords.java
@@ -0,0 +1,36 @@
+package stringclass;
+
+public class RearrangeSpacesBetweenWords {
+	// leetcode 1592 simple 2022/9/7
+	// ¼òµ¥Ä£Äâ¼´¿É
+    public String reorderSpaces(String text) {
+    	int length = text.length();
+        String[] words = text.trim().split("\\s+");
+        int cntSpace = length;
+        for (String word : words) {
+            cntSpace -= word.length();
+        }
+        StringBuilder sb = new StringBuilder();
+        if (words.length == 1) {
+            sb.append(words[0]);
+            for (int i = 0; i < cntSpace; i++) {
+                sb.append(' ');
+            }
+            return sb.toString();
+        }
+        int perSpace = cntSpace / (words.length - 1);
+        int restSpace = cntSpace % (words.length - 1);
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) {
+                for (int j = 0; j < perSpace; j++) {
+                    sb.append(' ');
+                }
+            }
+            sb.append(words[i]);
+        }
+        for (int i = 0; i < restSpace; i++) {
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+}
