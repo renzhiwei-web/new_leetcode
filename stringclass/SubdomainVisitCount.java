1092ec8e8e85fdb326c59985499819662eb83796
renzhiwei-web <renzhiwei565@163.com>
2022-10-05 08:47:09 +0800

    leetcode 811 模拟题

---
+++ stringclass/SubdomainVisitCount.java
@@ -0,0 +1,34 @@
+package stringclass;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SubdomainVisitCount {
+	// leetcode 811 medium 2022/10/5
+	// Ä£ÄâÌâ
+    public List<String> subdomainVisits(String[] cpdomains) {
+    	List<String> ans = new ArrayList<String>();
+    	Map<String, Integer> counts = new HashMap<>();
+    	for(String cpdomain : cpdomains) {
+    		int space = cpdomain.indexOf(' ');
+    		int count = Integer.parseInt(cpdomain.substring(0, space));
+    		String domain = cpdomain.substring(space + 1);
+    		counts.put(domain, counts.getOrDefault(domain, 0) + count);
+    		for(int i = 0;i < domain.length();i++) {
+    			if (domain.charAt(i) == '.') {
+					String subdomain = domain.substring(i + 1);
+					counts.put(subdomain, counts.getOrDefault(subdomain, 0) + count);
+				}
+    		}
+    		
+    	}
+    	for(Map.Entry<String, Integer> entry: counts.entrySet()) {
+    		String subdomain = entry.getKey();
+    		int count = entry.getValue();
+    		ans.add(count + " " + subdomain);
+    	}
+    	return ans;
+    }
+}
