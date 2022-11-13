e71a635f1182cdde3744e9824aceb76425f721f1
renzhiwei-web <renzhiwei565@163.com>
2022-11-13 09:34:42 +0800

    leetcode 791 简单模拟

---
+++ stringclass/CustomSortString.java
@@ -0,0 +1,29 @@
+package stringclass;
+
+
+public class CustomSortString {
+    // leetcode 791 medium 2022/11/13
+    // 简单进行模拟即可
+    public String customSortString(String order, String s) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            ++freq[ch - 'a'];
+        }
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < order.length(); ++i) {
+            char ch = order.charAt(i);
+            while (freq[ch - 'a'] > 0) {
+                ans.append(ch);
+                freq[ch - 'a']--;
+            }
+        }
+        for (int i = 0; i < 26; ++i) {
+            while (freq[i] > 0) {
+                ans.append((char) (i + 'a'));
+                freq[i]--;
+            }
+        }
+        return ans.toString();
+    }
+}
